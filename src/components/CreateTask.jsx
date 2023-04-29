afd4d8c0dbca4c95162c661bb16c038d3f0bacd0
Akshit Goyal <91284474+Goyal-Akshit@users.noreply.github.com>
2023-04-29 15:49:47 +0530

    Basis for task creation

---
+++ src/components/CreateTask.jsx
@@ -0,0 +1,5 @@
+export default function CreateTask() {
+  return (
+    <div>CreateTask</div>
+  )
+}
--- src/components/TaskList.jsx
+++ src/components/TaskList.jsx
@@ -1,7 +1,34 @@
+import React from "react";
 import Task from "./Task"
+import CreateTask from "./CreateTask";
 
 export default function TaskList() {
-  return (
-    <Task title="test" desc="test"></Task>
-  )
+	const [list, setList] = React.useState(
+	[
+		{
+			id: 1,
+			title: "test",
+			desc: "Excepteur aute elit esse elit mollit ad amet."
+		}
+	]);
+
+	
+	const listComp = list.map(listItem => {
+		return <Task
+			key={listItem.id}
+			title={listItem.title}
+			desc={listItem.desc}
+			handleEditFunction=""
+			handleDeleteFunction=""
+			/>
+	})
+
+	
+
+  	return (
+		<div>
+			<CreateTask></CreateTask>
+			{listComp}
+		</div>
+  	)
 }
