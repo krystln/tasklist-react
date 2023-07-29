e2b22cb34101d4c3569c7f2c930ff52dcb4d4303
Akshit Goyal <91284474+Goyal-Akshit@users.noreply.github.com>
2023-07-29 12:17:26 +0530

    undos added

---
+++ src/components/Promotion.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Promotion = () => {
+  return (
+    <div className='Promotion'>
+        <div>Created By : <a href="https://github.com/krystln">github@krystln</a></div>
+        <div>Portfolio : <a href="https://akshitgoyal.dev">akshitgoyal.dev</a></div>
+        <div>Twitter : <a href="https://twitter.com/krystlnwastaken">@krystlnwastaken</a></div>
+        <div>Email : <a href="mailto:akshitgoyal1025@gmail.com">akshitgoyal1025@gmail.com</a></div>
+    </div>
+  )
+}
+
+export default Promotion
